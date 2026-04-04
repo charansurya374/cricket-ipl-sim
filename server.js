16fda8212b0f0503221d8754d755c5bfc6167d10
charansurya374 <charansurya374@gmail.com>
2026-04-04 11:50:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,170 +1,203 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+// ================= TEAMS =================
+const teams = ["CSK","MI","RCB","KKR","SRH","DC","RR","PBKS","GT","LSG"];
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static(__dirname));
-
-// ===== TEAMS (10 IPL TEAMS) =====
-const teams = {
-  CSK: ["","Conway","Rahane","Dube","Dhoni","Jadeja","Moeen","Chahar","Deshpande","Theekshana","Pathirana"],
-  MI: ["","Rohit","Ishan","SKY","Tilak","Hardik","David","Wadhera","Bumrah","Coetzee","Chawla"],
-  RCB: ["","Kohli","Faf","Patidar","Maxwell","Green","DK","Lomror","Siraj","Topley","Karn"],
-  KKR: ["","Shreyas","Salt","Rinku","Russell","Narine","Nitish","Varun","Starc","Harshit","Ramandeep"],
-  SRH: ["","Head","Abhishek","Markram","Klaasen","Phillips","Samad","Cummins","Bhuvi","Natarajan","Markande"],
-  RR: ["","Jaiswal","Buttler","Samson","Parag","Hetmyer","Ashwin","Boult","Chahal","Avesh","Sandeep"],
-  DC: ["","Warner","Shaw","Marsh","Pant","Stubbs","Axar","Kuldeep","Nortje","Khaleel","Mukesh"],
-  GT: ["","Gill","Saha","Sai","Miller","Tewatia","Rashid","Shami","Noor","Mohit","Spencer"],
-  PBKS: ["","Dhawan","Bairstow","Livingstone","Jitesh","Shashank","Sam Curran","Rabada","Arshdeep","Rahul Chahar","Harpreet"],
-  LSG: ["","KL Rahul","de Kock","Stoinis","Pooran","Krunal","Ayush","Bishnoi","Wood","Yash","Naveen"]
+// ================= REAL PLAYERS =================
+let squad = {
+CSK:["Ruturaj Gaikwad","Conway","Rahane","Dube","Dhoni","Jadeja","Moeen","Chahar","Theekshana","Pathirana","Deshpande"],
+MI:["Rohit","Ishan","SKY","Tilak","Hardik","Tim David","Shepherd","Chawla","Bumrah","Coetzee","Madhwal"],
+RCB:["Kohli","Faf","Patidar","Maxwell","Green","DK","Lomror","Karn","Siraj","Topley","Dayal"],
+KKR:["Shreyas","Salt","Venky","Rana","Rinku","Russell","Narine","Ramandeep","Starc","Varun","Harshit"],
+SRH:["Head","Abhishek","Markram","Klaasen","Tripathi","Nitish","Shahbaz","Cummins","Bhuvi","Natarajan","Markande"],
+DC:["Warner","Shaw","Marsh","Pant","Stubbs","Axar","Lalit","Kuldeep","Nortje","Khaleel","Ishant"],
+RR:["Jaiswal","Buttler","Samson","Parag","Hetmyer","Jurel","Ashwin","Boult","Avesh","Chahal","Sandeep"],
+PBKS:["Dhawan","Bairstow","Livingstone","Curran","Jitesh","Shashank","Brar","Rabada","Arshdeep","Chahar","Harshal"],
+GT:["Gill","Saha","Sudharsan","Miller","Tewatia","Shankar","Rashid","Noor","Shami","Umesh","Spencer"],
+LSG:["Rahul","deKock","Stoinis","Pooran","Badoni","Hooda","Krunal","Bishnoi","Wood","Naveen","Mohsin"]
 };
 
-// ===== PLAYER =====
-let player = {
-  name: "",
-  team: "",
-  runs: 0,
-  balls: 0,
-  wickets: 0,
-  rating: 80
+// ================= SCHEDULE =================
+let schedule=[];
+for(let i=0;i<teams.length;i++){
+ for(let j=i+1;j<teams.length;j++){
+  schedule.push([teams[i],teams[j]]);
+ }
+}
+
+// ================= TABLE =================
+let table={};
+teams.forEach(t=>table[t]={pts:0});
+
+// ================= MATCH =================
+let matchIndex=0;
+
+let match={
+ batting:"",
+ bowling:"",
+ score:0,
+ wickets:0,
+ balls:0,
+ target:0,
+ innings:1,
+ striker:0,
+ nonStriker:1
 };
 
-// ===== GAME =====
-let game = {
-  score: 0,
-  wickets: 0,
-  balls: 0,
-  striker: 0,
-  nonStriker: 1
-};
+// ================= START =================
+function startMatch(){
+ let m=schedule[matchIndex];
+ match.batting=m[0];
+ match.bowling=m[1];
 
-// ===== SOCKET =====
-io.on("connection", (socket) => {
+ reset();
+ match.innings=1;
+ clearCommentary();
 
-  // START MATCH
-  socket.on("start", ({name, team}) => {
+ updateUI();
+}
 
-    player.name = name || "You";
-    player.team = team;
+function reset(){
+ match.score=0;
+ match.wickets=0;
+ match.balls=0;
+ match.striker=0;
+ match.nonStriker=1;
+}
 
-    teams[team][0] = player.name;
+// ================= PLAY BALL =================
+function playBall(){
 
-    // reset stats
-    player.runs = 0;
-    player.balls = 0;
-    player.wickets = 0;
-    player.rating = 80;
+ if(match.balls>=120 || match.wickets>=10){
+  endInnings();
+  return;
+ }
 
-    game = {
-      score: 0,
-      wickets: 0,
-      balls: 0,
-      striker: 0,
-      nonStriker: 1
-    };
+ let outcomes=[0,1,2,3,4,6,"W"];
+ let res=outcomes[Math.floor(Math.random()*outcomes.length)];
 
-    io.emit("update", game);
-    io.emit("career", player);
-  });
+ let strikerName = squad[match.batting][match.striker];
 
-  // PLAY BALL
-  socket.on("ball", () => {
+ let overBall = Math.floor(match.balls/6) + "." + (match.balls%6+1);
 
-    if(game.wickets >= 10 || game.balls >= 120){
-      io.emit("matchOver", "Match Finished");
-      return;
-    }
+ let text="";
 
-    let outcomes = [0,1,2,3,4,6,"W"];
-    let res = outcomes[Math.floor(Math.random()*outcomes.length)];
+ if(res==="W"){
+  match.wickets++;
+  text=`${overBall} ${strikerName} OUT! 😱`;
+  match.striker++;
+ }
+ else{
+  match.score+=res;
 
-    let batsman = teams[player.team][game.striker];
+  if(res===0) text=`${overBall} ${strikerName} dot ball`;
+  if(res===4) text=`${overBall} ${strikerName} FOUR! 🔥`;
+  if(res===6) text=`${overBall} ${strikerName} SIX! 🚀`;
+  if(res===1||res===2||res===3) text=`${overBall} ${strikerName} ${res} run(s)`;
 
-    let over = Math.floor(game.balls / 6);
-    let ballNo = game.balls % 6 + 1;
+  if(res%2===1){
+   [match.striker,match.nonStriker]=[match.nonStriker,match.striker];
+  }
+ }
 
-    let commentators = ["Harsha Bhogle","Ravi Shastri","Ian Bishop"];
-    let comm = commentators[Math.floor(Math.random()*3)];
+ match.balls++;
 
-    let text = "";
+ if(match.balls%6===0){
+  [match.striker,match.nonStriker]=[match.nonStriker,match.striker];
+ }
 
-    // ===== RESULT LOGIC =====
-    if(res === "W"){
-      game.wickets++;
+ addCommentary(text);
 
-      text = `${comm}: ${over}.${ballNo} ${batsman} goes up in the air... TAKEN!`;
+ if(match.innings===2 && match.score>=match.target){
+  endMatch(match.batting);
+ }
 
-      io.emit("highlight", text);
-      io.emit("wicket");
+ updateUI();
+}
 
-      game.striker = (game.striker + 1) % 11;
+// ================= END INNINGS =================
+function endInnings(){
 
-    } else {
+ if(match.innings===1){
 
-      game.score += res;
+  match.target=match.score+1;
 
-      if(batsman === player.name){
-        player.runs += res;
-        player.balls++;
-      }
+  [match.batting,match.bowling]=[match.bowling,match.batting];
 
-      // commentary
-      if(res === 0){
-        text = `${comm}: ${over}.${ballNo} Dot ball.`;
+  reset();
+  match.innings=2;
 
-      } else if(res === 1){
-        text = `${comm}: ${over}.${ballNo} Quick single.`;
+ } else {
 
-      } else if(res === 2){
-        text = `${comm}: ${over}.${ballNo} Good running, 2 runs.`;
+  if(match.score>=match.target){
+   endMatch(match.batting);
+  } else {
+   endMatch(match.bowling);
+  }
+ }
+}
 
-      } else if(res === 3){
-        text = `${comm}: ${over}.${ballNo} Excellent running, 3 runs!`;
+// ================= END MATCH =================
+function endMatch(winner){
+ table[winner].pts+=2;
 
-      } else if(res === 4){
-        text = `${comm}: ${over}.${ballNo} Cracking shot! FOUR!`;
-        io.emit("highlight", text);
+ alert("Winner: "+winner);
 
-      } else if(res === 6){
-        text = `${comm}: ${over}.${ballNo} UP IN THE AIR... SIX!!!`;
-        io.emit("highlight", text);
-        io.emit("six");
-      }
+ matchIndex++;
 
-      // strike rotation
-      if(res % 2 === 1){
-        let temp = game.striker;
-        game.striker = game.nonStriker;
-        game.nonStriker = temp;
-      }
-    }
+ if(matchIndex>=schedule.length){
+  alert("League Finished!");
+ } else {
+  startMatch();
+ }
+}
 
-    game.balls++;
+// ================= AUTO =================
+function autoPlay(){
+ let i=setInterval(()=>{
+  if(match.balls>=120) clearInterval(i);
+  else playBall();
+ },120);
+}
 
-    // YOU BOWL RANDOMLY
-    if(Math.random() < 0.2 && res === "W"){
-      player.wickets++;
-    }
+function simulateInnings(){
+ while(match.balls<120){
+  playBall();
+ }
+}
 
-    // LEVEL SYSTEM
-    if(player.runs > 50) player.rating++;
-    if(player.wickets > 2) player.rating++;
+function simulateMatch(){
+ simulateInnings();
+ simulateInnings();
+}
 
-    // SEND DATA
-    io.emit("update", game);
-    io.emit("career", player);
-    io.emit("commentary", text);
+// ================= COMMENTARY =================
+function addCommentary(text){
+ let div=document.getElementById("commentary");
+ div.innerHTML = text + "<br>" + div.innerHTML;
+}
 
-  });
+function clearCommentary(){
+ document.getElementById("commentary").innerHTML="";
+}
 
-});
+// ================= UI =================
+function updateUI(){
 
-// ===== SERVER =====
-const PORT = process.env.PORT || 3000;
+ document.getElementById("match").innerHTML=`
+ <h2>${match.batting} vs ${match.bowling}</h2>
+ <h1>${match.score}/${match.wickets}</h1>
+ <p>Overs: ${(match.balls/6).toFixed(1)}</p>
+ <p>Striker: ${squad[match.batting][match.striker] || "-"}</p>
+ <p>Non-Striker: ${squad[match.batting][match.nonStriker] || "-"}</p>
+ ${match.innings===2?`<p>Target: ${match.target}</p>`:""}
+ `;
 
-server.listen(PORT, () => {
-  console.log("Server running...");
-});
+ let html="<h2>Points Table</h2>";
+ teams.forEach(t=>{
+  html+=`<p>${t} - ${table[t].pts}</p>`;
+ });
+
+ document.getElementById("table").innerHTML=html;
+}
+
+// ================= INIT =================
+startMatch();
